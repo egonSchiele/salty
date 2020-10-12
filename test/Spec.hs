f5a5996867c7d976b30fe76beb0f838f2d414930
Aditya Bhargava <bluemangroupie@gmail.com>
2020-10-12 17:52:51 -0500

    6 specs currently pass

--- test/Spec.hs
+++ test/Spec.hs
@@ -56,12 +56,12 @@ phpBlob = [r|
 longerTest = saltyBlob `matches` phpBlob
 
 transpileTests = [
-   -- "foo = 1" `matches` "$foo = 1",
-   --  "@foo = 1" `matches` "$this->foo = 1",
-   --  "@@foo = 1" `matches` "self::$foo = 1",
-    -- "build a b := 2" `matches` "function build($a, $b) {\nreturn 2;\n}",
-    "build a b := return 2" `matches` "function build($a, $b) {\nreturn 2;\n}"
-    -- "@@build a b := 2" `matches` "static function build($a, $b) {\nreturn 2;\n}"
+   "foo = 1" `matches` "$foo = 1",
+    "@foo = 1" `matches` "$this->foo = 1",
+    "@@foo = 1" `matches` "self::$foo = 1",
+    "build a b := 2" `matches` "function build($a, $b) {\nreturn 2;\n}",
+    "build a b := return 2" `matches` "function build($a, $b) {\nreturn 2;\n}",
+    "@@build a b := 2" `matches` "static function build($a, $b) {\nreturn 2;\n}"
     -- "fib x := return x if x < 2" `matches` "function fib($x) {\nif ($x < 2) {\nreturn $x;\n}"
     -- "@@foo a b := @@bar(b)" `matches` "static function foo($a, $b) {\n\treturn static::bar($b);\n}",
     -- "# hi" `matches` "// hi",
