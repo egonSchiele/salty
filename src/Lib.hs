246cb79e2f508205b63481487c58f1eb1fe0bf23
Adit <adit@adit.io>
2020-10-12 04:45:57 +0000

    bam parsec added

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,4 +1,14 @@
 module Lib where
 
+import Text.Parsec (parse)
+import Text.ParserCombinators.Parsec.Char (anyChar)
+import Text.Parsec.Combinator (many1)
+import Text.ParserCombinators.Parsec.Error(messageString)
+
+
 build :: String -> String
-build str = "hi"
+build str = case (parse numbers str "hi") of
+                 Left err -> show err
+                 Right xs -> xs
+
+numbers = many1 anyChar
