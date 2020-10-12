95f60add745d3b99ed705976999bb280288073e5
Aditya Bhargava <bluemangroupie@gmail.com>
2020-10-12 01:06:20 -0500

    spec added for bug

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,7 +10,8 @@ matches str1 str2 = TestCase $ assertEqual "" str2 (build str1)
 tests = TestList [
     "@foo = 1" `matches` "$this->foo = 1",
     "@@foo = 1" `matches` "self::$foo = 1",
-    "build a b := 2" `matches` "function build($a, $b) {\nreturn 2;\n}"
+    "build a b := 2" `matches` "function build($a, $b) {\nreturn 2;\n}",
+    "build a b := return 2" `matches` "function build($a, $b) {\nreturn 2;\n}"
     -- "@@build a b := 2" `matches` "static function build($a, $b) {\n\treturn 2;\n}",
     -- "@@foo a b := @@bar(b)" `matches` "static function foo($a, $b) {\n\treturn static::bar($b);\n}",
     -- "# hi" `matches` "// hi",
