a396e7a843fda2969bbfb58500607d825d3c9b5f
Aditya Bhargava <bluemangroupie@gmail.com>
2020-10-16 20:20:43 -0500

    tests for function type sigs

--- test.php
+++ test.php
@@ -1,10 +1,7 @@
 <?php
-public function __construct($a) {
-    $this->a = $a;
-}
-public function myPubFunc() {
-    var_dump("asd");
-}
-private function myPriFunc() {
-    var_dump("asd");
+/**
+ * @param String
+ */
+public function foo(String $a) {
+    $a;
 }
