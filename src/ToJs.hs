78219fd7e410f8282a3d34108455e1ffd6d1e091
Aditya Bhargava <bluemangroupie@gmail.com>
2021-02-15 15:29:21 -0600

    needed for tag functions

--- src/ToJs.hs
+++ src/ToJs.hs
@@ -383,6 +383,7 @@ addReturn x@(SaltyString _) = "return " ++ (toJs x)
 addReturn x@(SaltyOptional salty) = "return " ++ (toJs x)
 addReturn x@(SaltyBool _) = "return " ++ (toJs x)
 addReturn x@(PurePhp _) = "return " ++ (toJs x)
+addReturn x@(Function _ _ _ _ _) = "return " ++ (toJs x)
 addReturn x@(SaltyGuard (Just val) guards) = toJs (SaltyGuard (Just val) newGuards)
   where newGuards = map addReturn_ guards
         addReturn_ (Guard cond outcome) = Guard cond ((init outcome) ++ [ReturnStatementForAddReturn (last outcome)])
